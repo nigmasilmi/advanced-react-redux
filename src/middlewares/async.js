074089cfd3900fd9883c4591278b0ddaed2155a9
nigma <nigma@teroapp.com>
2021-02-18 20:05:06 -0300

    Async and stateValidator middlewares implemented

---
+++ src/middlewares/async.js
@@ -0,0 +1,18 @@
+const funka = ({ dispatch }) => (next) => (action) => {
+  // check to see if the action has a promise on its payload promise
+  // if it does then wait for it to resolve
+  // if it does not then send the action on to the next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+
+  // if it does wait for the promise to resolve
+  // get its data and then create a new action with that data and dispatch it
+
+  action.payload.then((response) => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
+};
+
+export default funka;
